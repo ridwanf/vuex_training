1334171ff69cd5fc18da872715e3f467b5da171a
DESKTOP-96V209H\ridwan <ridwanfansuri@gmail.com>
2018-10-09 16:06:41 +0700

    add mapMutations

--- src/components/CounterStore.vue
+++ src/components/CounterStore.vue
@@ -14,6 +14,17 @@ export default {
     decrement () {
       this.$store.commit('decrement')
     },
+    // example use mapMutations
+    // ...mapMutations([
+    //   'increment', // map `this.increment()` to `this.$store.commit('increment')`
+
+    // `mapMutations` also supports payloads:
+    //   'incrementBy' // map `this.incrementBy(amount)` to `this.$store.commit('incrementBy', amount)`
+    // ]),
+    // use different name
+    // ...mapMutations({
+    //   add: 'increment' // map `this.add()` to `this.$store.commit('increment')`
+    // })
   },
 }
 </script>
